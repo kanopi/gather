604509fa90e5097a70ffcbbd86d421d2a3b738f1
Katherine White <katherine@kanopistudios.com>
2017-11-09 10:35:45 -0600

    promisifying request logic.

--- gathered.js
+++ gathered.js
@@ -1,22 +1,29 @@
+var Promise = require('bluebird');
 
 var request = require('request-promise'),
     errors = require('request-promise/errors'),
     cheerio = require('cheerio'),
     papa = require('papaparse'),
-    prompt = require('prompt'),
+    prompt =  require('prompt'),
     fs = require('fs'),
     csvWriter = require('csv-write-stream');
 
 var buildContent = {
   data : [],
   schema : {},
+  urlStream : '',
+  pageList : [],
   init : function() {
+    // promises promises
     this.getUserInput();
+    //Promise.all(main).then(this.writeContent());
   },
   getUserInput: function() {
     // get the CSV file path to parse from the user.
     var inputSchema = {
       properties: {
+        // would be _nice_ to make sure this exists and throw an error if not.
+        // but this is a developer utility for a project at the moment.
         filename: {
           description: "Please enter the file path"
         },
@@ -25,66 +32,83 @@ var buildContent = {
         }
       }
     };
+    var that = this;
     prompt.start();
     prompt.get(inputSchema, function (err, inputSchema) {
         if (err) { return onErr(err); }
 
         // load file to a string. need to add error handling.
-        buildContent.schema = inputSchema;
+        that.schema = inputSchema;
         var file = fs.readFileSync(inputSchema.filename, 'UTF-8');
-        // parse CSV.
-        buildContent.parsePages(file);
+        that.urlStream = file;
+        that.parsePages();
+        // chainable
+        return this;
     });
   },
-  parsePages : function(file) {
-    papa.parse(file, {
+  parsePages : function() {
+    var that = this;
+
+    papa.parse(this.urlStream, {
       header: true,
       complete: function(results) {
         if (results.errors.length) {
           onErr(results.errors);
         }
         console.log('Getting CSV Contents:',results.data.length,'items found.');
-        // grab content based on URLs in the file.
-        buildContent.getPages(results.data);
+        that.pageList = results.data;
+        that.getPages();
     	}
     });
 
-    return true;
+    return this;
   },
-  getPages : function (rows) {
+  getPages : function () {
     console.log('Accessing page contents.');
+    // this has to use promises.
+    var that = this;
+    var requests = [];
+    for(var i = 0, len = this.pageList.length; i < len; i++) { // stubbing out count since we have a lot of records.
+        var row = this.pageList[i];
+        var options = {
+          uri: row.URL,
+          transform: function(body) {
+            return cheerio.load(body);
+          }
+        };
+        requests.push(
+          request(options)
+          .then(function($) {
+            var content = $('#maincol');
+            // make sure these things exist before we try to clean them up.
+            var item = {
+              type : that.schema.type
+            };
+            var title = content.find('h1').first().text();
+            var date = content.find('.author').first().text();
+            var body = content.find('.blog-body').html();
+            if (title) {
+              item.title = that.scrub(title);
+            }
+            if (date) {
+              item.date = that.scrub(date);
+            }
+            if (body) {
+              item.body = body.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
+            }
+            //console.log(item);
+            that.data.push(item);
+          })
+        );
+    } // end for.
 
-    for(var i = 0, len = rows.length; i<1; i++) { // stubbing out count since we have a lot of records.
-      var row = rows[i];
-      var options = {
-      	uri: row.URL,
-      	transform: function(body) {
-      		return cheerio.load(body);
-      	}
-      };
-
-      request(options)
-      	.then(function($) {
-      		var content = $('#maincol');
-          var item = {
-            type: buildContent.schema.type,
-            //url: url,
-            'title': buildContent.scrub( content.find('h1').first().text() ),
-            'dateline': buildContent.scrub( content.find('.author').first().text() ),
-            'content': content.find('.blog-body').html().replace(/[\x00-\x1F\x7F-\x9F]/g, "")
-          };
-          console.log(item);
-      		buildContent.data.push(item);
-      	})
-        .catch( function(err) {onErr(err);} )
-        .finally(function() {
-          // if all requests are zeroed out, write to file.
-
-        })
-    }
+    Promise.all(requests).then(function() {
+      that.writeContent();
+    })
+    .catch(function(e){throw e;});
   },
   writeContent : function () {
-    //console.log('writeContent:', buildContent.data);
+    console.log('writeContent:', this.data);
     //console.log(JSON.stringify());
   },
   scrub : function (content) {
