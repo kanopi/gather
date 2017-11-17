d791eebd4b7fd43a786030dee37b67ab1984d267
Katherine White <katherine@kanopistudios.com>
2017-11-17 17:28:58 -0600

    the trouble with dates...

--- gathered.js
+++ gathered.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird');
 
-var request = require('request-promise'),
+var yaml = require('js-yaml'),
+    request = require('request-promise'),
     errors = require('request-promise/errors'),
     cheerio = require('cheerio'),
     papa = require('papaparse'),
@@ -9,6 +10,7 @@ var request = require('request-promise'),
     csvWriter = require('csv-write-stream');
 
 var buildContent = {
+  config : '',
   data : [],
   schema : {},
   urlStream : '',
@@ -21,7 +23,7 @@ var buildContent = {
   },
   getUserInput: function() {
     // get the CSV file path to parse from the user.
-    var inputSchema = {
+    var inputs = {
       properties: {
         // would be _nice_ to make sure this exists and throw an error if not.
         // but this is a developer utility for a project at the moment.
@@ -30,20 +32,28 @@ var buildContent = {
         },
         type: {
           description: "Please enter the slug for the post type"
+        },
+        /*
+        config: {
+          description: "Please enter the path to your YAML file"
         }
+        */
       }
     };
     var that = this;
     prompt.start();
-    prompt.get(inputSchema, function (err, inputSchema) {
+    prompt.get(inputs, function (err, inputs) {
         if (err) { return onErr(err); }
 
         // load file to a string. need to add error handling.
-        that.schema = inputSchema;
-        var file = fs.readFileSync(inputSchema.filename, 'UTF-8');
-        that.urlStream = file;
+        that.schema = inputs;
+
+        if (that.config) {
+          that.config = yaml.safeLoad(fs.readFileSync(inputs.config, 'utf8'));
+        }
+        var file = that.urlStream = fs.readFileSync(inputs.filename, 'utf-8');
         that.parsePages();
-        // chainable
+
         return this;
     });
   },
@@ -70,43 +80,53 @@ var buildContent = {
     var that = this;
     var requests = [];
     for(var i = 0, len = this.pageList.length; i < len; i++) {
-        var row = this.pageList[i];
-        var options = {
-          uri: row.URL,
-          transform: function(body) {
-            return cheerio.load(body);
+
+      var row = this.pageList[i];
+
+      // with no URL, we have no reason to be here.
+      if ( !row.URL.length ) {
+        continue;
+      }
+
+      var options = {
+        uri: row.URL,
+        rowMeta: row,
+        transform: function(body) {
+          return cheerio.load(body);
+        }
+      };
+      requests.push(
+        request(options)
+        .then(function($) {
+          // ideally we move this (and the file details) into some kind of
+          // config so we can reuse this script and lose the prompt...
+
+          // we need to find our row data again - cleanest way to grab some meta info.
+          var content = $('#maincol'),
+              item = {
+                postType : that.schema.type,
+                pubDate : options.rowMeta.pubDate,
+                seoTitle : options.rowMeta.Title,
+                seoDesc : options.rowMeta.Description,
+              };
+
+          var title = content.find('h1').first().text(),
+              date = content.find('.author').first().text(),
+              body = content.find('.blog-body').html();
+
+          if (title) {
+            item.title = that.scrub(title);
           }
-        };
-        requests.push(
-          request(options)
-          .then(function($) {
-            // ideally we move this (and the file details) into some kind of
-            // config so we can reuse this script and lose the prompt...
-            var content = $('#maincol'),
-                item = {
-                  postType : that.schema.type,
-                  seoTitle : row.Title,
-                  seoDate : row.Date,
-                  seoDesc : row.Description,
-                };
-
-            var title = content.find('h1').first().text(),
-                date = content.find('.author').first().text(),
-                body = content.find('.blog-body').html();
-
-            if (title) {
-              item.title = that.scrub(title);
-            }
-            if (date) {
-              item.date = that.scrub(date);
-            }
-            if (body) {
-              item.body = body.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
-              item.body = that.scrub(body);
-            }
-            that.data.push(item);
-          })
-        );
+          if (date) {
+            item.byline = that.scrub(date);
+          }
+          if (body) {
+            //item.body = body.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
+            item.body = that.scrub(body);
+          }
+          that.data.push(item);
+        })
+      );
     } // end loop.
 
     Promise.all(requests).then(function() {
@@ -115,7 +135,9 @@ var buildContent = {
     .catch(function(e){throw e;});
   },
   writeContent : function () {
-    var writer = csvWriter(),
+    var writer = csvWriter(
+      { headers : Object.keys(this.data[0]) }
+    ),
         filename = this.schema.type + '-content.csv';
     writer.pipe(fs.createWriteStream(filename));
     // this can't be right... but it works.
